f48bc1f0ad1b347862f4fe2bcbad23e847e0a5b9
Apurbaroy01 <apurbaroy00011@gmail.com>
2025-08-17 20:41:56 +0600

    19th

--- src/Firebase/Context/AuthProvider.jsx
+++ src/Firebase/Context/AuthProvider.jsx
@@ -7,39 +7,49 @@ import axios from 'axios';
 
 
 const AuthProvider = ({ children }) => {
-    const [user, setUser]=useState(null)
-    const [loading, setLoading]=useState(true)
+    const [user, setUser] = useState(null)
+    const [loading, setLoading] = useState(true)
 
 
     const createUser = (email, password) => {
         setLoading(true)
         return createUserWithEmailAndPassword(auth, email, password)
-        
+
     };
 
     const signIn = (email, password) => {
         setLoading(true)
         return signInWithEmailAndPassword(auth, email, password)
-        .finally(() => setLoading(false));
+            .finally(() => setLoading(false));
     };
-    const logout =()=>{
+    const logout = () => {
         setLoading(true)
-       return signOut(auth)
+        return signOut(auth)
     }
 
     useEffect(() => {
-        const unsubscribe = onAuthStateChanged(auth,(currentUser)=>{
-            console.log('LoginUser',currentUser?.email)
+        const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+            console.log('LoginUser', currentUser?.email)
             setUser(currentUser);
 
-            if(currentUser?.email){
-                const user= {email: currentUser.email}
+            if (currentUser?.email) {
+                const user = { email: currentUser.email }
 
-                axios.post('http://localhost:5000/jwt', user,{
+                axios.post('http://localhost:5000/jwt', user, {
+                    withCredentials: true
+                })
+                    .then(res => {
+                        console.log(res.data)
+                        setLoading(false)
+                    })
+            }
+            else {
+                axios.post('http://localhost:5000/logout', {}, {
                     withCredentials: true
                 })
                 .then(res=>{
                     console.log(res.data)
+                    setLoading(false)
                 })
             }
 
